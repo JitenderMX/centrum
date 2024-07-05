0c46f7f251b57817e9bcf775726820cb0185ea10
JitenderMX <jitender.work.mediax@gmail.com>
2024-07-05 17:41:25 +0530

    final changes

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ AOS.init();
 $("#header").load("header.html");
 $("#footer").load("footer.html");
 $(window).on('load', function () {
-    $('#about-page .nav-logo img, #product-sub-page .nav-logo img').attr('src', 'image/logo-dark.png');
+    $('#about-page .nav-logo img, #product-sub-page .nav-logo img, #product-page .nav-logo img').attr('src', 'image/logo-dark.png');
 });
 $(window).scroll(function () {
     let st = $(window).scrollTop();
     if (st > 0) {
         $(".ah-img").addClass("active");
-        $('#about-page .nav-logo img, #product-sub-page .nav-logo img').attr('src', 'image/logo-light.png');
+        $('#about-page .nav-logo img, #product-sub-page .nav-logo img,  #product-page .nav-logo img').attr('src', 'image/logo-light.png');
     } else {
         $(".ah-img").removeClass("active");
-        $('#about-page .nav-logo img, #product-sub-page .nav-logo img').attr('src', 'image/logo-dark.png');
+        $('#about-page .nav-logo img, #product-sub-page .nav-logo img,  #product-page .nav-logo img').attr('src', 'image/logo-dark.png');
     }
 })
